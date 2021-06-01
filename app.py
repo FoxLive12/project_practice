19d1ce7b592dd5d0656c6e53ca7beaf63dade081
FoxLive <malygin.d.i.2.18@gmail.com>
2021-06-01 15:58:02 +0300

    Start project

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
 from flask import Flask, render_template
+from config import Configuration
 
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-if __name__=="__main__":
-	app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,3 @@
+class Configuration(object):
+    DEBUG = True
+    SECRET_KEY = 'h2T//xcf/x12[/xc4/x13&/xcd/xc0/xeb;B/x7f/xc1/x18z/x99/xd1/x90/xd4'
